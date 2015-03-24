3bd15d0873fa80e44379dbdd6de011a6c30eb1d6
Adam Doan <doana@uoguelph.ca>
2015-03-24 16:01:44 -0400

    Added title attribute for file upload.

--- views/shared/pdf-embed-js/web/viewer.js
+++ views/shared/pdf-embed-js/web/viewer.js
@@ -5402,6 +5402,7 @@ function webViewerInitialized() {
   fileInput.id = 'fileInput';
   fileInput.className = 'fileInput';
   fileInput.setAttribute('type', 'file');
+  fileInput.setAttribute('title', 'Upload file');
   fileInput.oncontextmenu = noContextMenuHandler;
   document.body.appendChild(fileInput);
 
